fd896c76c46a54af50904ac6494456c09283cfe7
=abearxiong <xiongxiao1012@outlook.com>
2019-09-15 23:01:49 +0800

    对过去的升一个级，同时准备把这个库托管为网页

---
+++ build/precache-manifest.738428a4c3911d5cbfabe6491c80b217.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bece36dfa093739b2dab118003faeb7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20cca51251f8de3eae8f",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5e28032f4b27649ddf1f",
+    "url": "./static/css/main.f8f07599.chunk.css"
+  },
+  {
+    "revision": "20cca51251f8de3eae8f",
+    "url": "./static/js/2.e546c14b.chunk.js"
+  },
+  {
+    "revision": "5e28032f4b27649ddf1f",
+    "url": "./static/js/main.b2f647ef.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
