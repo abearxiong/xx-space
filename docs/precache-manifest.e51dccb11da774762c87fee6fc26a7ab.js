e5fe8174ef303b0340dbbe19018a4a1fa9f577e2
abearxiong bd <xiongxiao1012@outlook.com>
2020-01-06 17:43:51 +0800

    static

---
+++ docs/precache-manifest.e51dccb11da774762c87fee6fc26a7ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d2e4208f8b792479ab02fbb5c05a11",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d54674835fc53081aed6",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e1838837b4c64a01cf53",
+    "url": "./static/css/main.b00f828b.chunk.css"
+  },
+  {
+    "revision": "d54674835fc53081aed6",
+    "url": "./static/js/2.670ef664.chunk.js"
+  },
+  {
+    "revision": "ef8b65a2f82c7e6c0d922282393b9708",
+    "url": "./static/js/2.670ef664.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1838837b4c64a01cf53",
+    "url": "./static/js/main.3bf88df3.chunk.js"
+  },
+  {
+    "revision": "2082ed2c9115ac8848b9",
+    "url": "./static/js/runtime-main.edfdb079.js"
+  }
+]);
